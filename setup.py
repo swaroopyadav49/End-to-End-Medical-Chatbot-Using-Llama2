c1f43260895b9612d7c5335d0fae8808a375e1a3
swaroop <swaroopyadav49@gmail.com>
2024-04-23 22:30:38 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Medical Chatbot',
-    version= '0.0.1',
-    author= 'swaroop',
-    author_email= 'swaroopyadav49@gmail.com',
-    install_requires = ["ctransformers==0.2.5","sentence-transformers==2.2.2","pinecone-client","langchain==0.0.225",
-                         "flask","pypdf","python-dotenv"]
-    packages= find_packages()
+    name='Medical Chatbot',
+    version='0.0.1',
+    author='swaroop',
+    author_email='swaroopyadav49@gmail.com',
+    install_requires=["ctransformers==0.2.5","sentence-transformers==2.2.2","pinecone-client","langchain==0.0.225",
+                         "flask","pypdf","python-dotenv"],
+    packages=find_packages()
+
 )
